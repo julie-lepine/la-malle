094a9d3e2a9e9bc780cf8b5192407cc3f98e696c
julie-lepine <lepine.julie@live.fr>
2023-07-26 18:41:37 +0200

    fixing issue with img links

--- assets/js/boardgames.js
+++ assets/js/boardgames.js
@@ -48,6 +48,6 @@ const puzzImg = document.getElementById("puzzImg");
 const puzzSelectBtns = document.getElementById("puzzleSelect");
 
 function displayImg() {
-  diceImg.src = `../../assets/img/board_games/dice-${diceSelectBtns.value}.jpg`;
-  puzzImg.src = `../../assets/img/board_games/puzz-${puzzSelectBtns.value}.jpg`;
+  diceImg.src = `../assets/img/board_games/dice-${diceSelectBtns.value}.jpg`;
+  puzzImg.src = `../assets/img/board_games/puzz-${puzzSelectBtns.value}.jpg`;
 }
